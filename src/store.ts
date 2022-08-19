bec908c28a9888fcf76d8febef0669f9a01c2414
Nati82 <ashenafinati82@gmail.com>
2022-08-19 23:33:44 +0300

    added actions, reducer, selectors, types and store

---
+++ src/store.ts
@@ -0,0 +1,6 @@
+import { rootReducer } from "./rootReducer";
+import { composeWithDevTools, devToolsEnhancer } from "redux-devtools-extension";
+import { applyMiddleware, createStore } from "redux";
+import { logger } from "redux-logger";
+
+export const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(logger)));
