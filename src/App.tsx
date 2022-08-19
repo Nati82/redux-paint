10c661a8b66471c09f92e343d21e283c9c489bfb
Nati82 <ashenafinati82@gmail.com>
2022-08-20 00:05:04 +0300

    implemented mouse events in app.tsx and added a draw stroke method

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,46 @@
-import React, { useRef } from "react";
+import React, { useEffect, useRef } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { beginStroke, endStroke, updateStroke } from "./actions";
+import { drawStroke } from "./canvasUtils";
+import { currentStrokeSelector } from "./selectors";
 
 function App() {
   const canvasRef = useRef<HTMLCanvasElement>(null);
+  const currentStroke = useSelector(currentStrokeSelector);
+  const dispatch = useDispatch();
+  const isDrawing = !!currentStroke.points.length;
+  const getCanvasWithContext = (canvas = canvasRef.current) => {
+    return { canvas, context: canvas?.getContext("2d") };
+  };
 
-  const startDrawing = () => {};
-  const endDrawing = () => {};
-  const draw = () => {};
+  useEffect(() => {
+    const { context } = getCanvasWithContext();
+    if (!context) {
+      return;
+    }
+    requestAnimationFrame(() =>
+      drawStroke(context, currentStroke.points, currentStroke.color)
+    );
+  }, [currentStroke]);
+
+  const startDrawing = ({
+    nativeEvent,
+  }: React.MouseEvent<HTMLCanvasElement>) => {
+    const { offsetX, offsetY } = nativeEvent;
+    dispatch(beginStroke(offsetX, offsetY));
+  };
+  const endDrawing = () => {
+    if (isDrawing) {
+      dispatch(endStroke());
+    }
+  };
+  const draw = ({ nativeEvent }: React.MouseEvent<HTMLCanvasElement>) => {
+    if (!isDrawing) {
+      return;
+    }
+    const { offsetX, offsetY } = nativeEvent;
+    dispatch(updateStroke(offsetX, offsetY));
+  };
 
   return (
     <canvas
--- src/store.ts
+++ src/store.ts
@@ -2,5 +2,6 @@ import { rootReducer } from "./rootReducer";
 import { composeWithDevTools, devToolsEnhancer } from "redux-devtools-extension";
 import { applyMiddleware, createStore } from "redux";
 import { logger } from "redux-logger";
+// import { configureStore } from "redux"
 
 export const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(logger)));
